20062c3beb34d3af1179bc6e9f16404f848cb9b3
rizki <rizkiadnanfutuh@gmail.com>
2025-08-11 07:42:34 +0700

    feat: add routing setup with authentication redirect

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./pages/auth/login";
+import SignUp from "./pages/auth/SignUp";
+import Home from "./pages/Dashboard/home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signUp" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
+
+const Root = () => {
+  // Check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // Redirect to Dashboard if authenticated, Otherwise to login
+  return isAuthenticated ? <Navigate to={"/dashboard"} /> : <Navigate to={"/login"} />;
+};
